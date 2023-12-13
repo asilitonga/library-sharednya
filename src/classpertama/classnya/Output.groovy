8c0a5fa09783e2c54bccf8d8e1cf489a2ed40dc3
Andreas Silitonga <echo.andreassilitonga586@gmail.com>
2023-12-14 01:59:02 +0700

    edit 6 file library 1

---
+++ src/classpertama/classnya/Output.groovy
@@ -0,0 +1,10 @@
+//kita panggil dulu namafolder project kita
+package classpertama.classnya;
+
+//buat classnya dulu
+//baru static def
+class Output {
+    static def hello(String name) {
+        println("Hello ${name}")
+    }
+}
