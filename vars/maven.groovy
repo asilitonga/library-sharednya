23254cfb42ef954db2c298cd58350ed974af8171
Andreas Silitonga <echo.andreassilitonga586@gmail.com>
2023-12-14 12:35:57 +0700

    edit 2 file library 4

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,5 +1,11 @@
 //menggunakan parameter
 //kita bisa menjalankan file yg kita mau dan tentukan berdasarkan parameternya difolder: vars
 //didalam parameter kita bisa tambahkan parameter lainnya
-def call(String command)//didalam kurung isinya jika ditambah parameter lainnya (String command, parameter2, parameter3) {
+def call(List command) {//didalam kurung isinya jika ditambah parameter lainnya (String command, parameter2, parameter3) {
+    //membuat variabel baru yaitu commands yg akan di redirect nyimpan datanya ke variabel aslinya yaitu: command
+    //kemudian function comandnya akan dijalankan dengan instruksi: sh
+    //tentukan function > buat perulangan > perulangannya itu menjalankan suatu file/instruksi
+    for (command in commands){   
     sh("./mvnw ${command}")
+    }
+}
